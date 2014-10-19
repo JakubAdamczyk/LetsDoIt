992c6218267afe1b56579c5f8a162c89675e486b
Jakub Adamczyk <andrzej@golonka.pl>
2014-10-19 12:32:07 +0200

    add validations to models

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ActiveRecord::Base
   has_many :lists
   has_many :tasks, through: :lists
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
 end

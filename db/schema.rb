cd7b14914659afe156ac39f33c163cc1c66bba32
Jakub Adamczyk <andrzej@golonka.pl>
2014-10-19 11:42:11 +0200

    create models 'list,user,task'  and migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20141019085645) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "lists", force: true do |t|
+    t.integer  "user_id"
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "tasks", force: true do |t|
+    t.integer  "list_id"
+    t.string   "name"
+    t.boolean  "status"
+    t.date     "execution_date"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end

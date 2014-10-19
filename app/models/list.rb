502c90caf11b5aa0f278f3c7724cfe4c57390de4
Jakub Adamczyk <andrzej@golonka.pl>
2014-10-19 16:43:59 +0200

    fix problem with default list settings

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,7 +2,7 @@ class List < ActiveRecord::Base
   belongs_to :user
   has_many :tasks
 
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true
 
   DEFAULT_NAME = "Default List"
 end

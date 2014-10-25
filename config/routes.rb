4bd2c52526373d06aa7e67200b1c14009243e985
Jakub Adamczyk <andrzej@golonka.pl>
2014-10-25 16:54:41 +0200

    fix home page

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
   resources :lists do 
     resources :tasks, only: [:new, :show, :create, :index]
   end
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
